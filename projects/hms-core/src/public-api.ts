ece62d354778f863a53e4d7810b8ff14a37bd054
Sang Nguyen <sang.nguyen@orientsoftware.net>
2019-05-09 16:46:10 +0700

    Update on missing code from old repository

--- projects/hms-core/src/public-api.ts
+++ projects/hms-core/src/public-api.ts
@@ -2,8 +2,9 @@
  * Public API Surface of hms-core
  */
 
-export * from './lib/hms-core.service';
-export * from './lib/hms-core.component';
 export * from './lib/hms-core.module';
 
 export * from './lib/header/header.component';
+export * from './lib/footer/footer.component';
+export * from './lib/login/login.component';
+export * from './lib/sidebar/sidebar.component';
